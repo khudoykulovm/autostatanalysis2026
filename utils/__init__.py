b4f1ac22a70972416eb4a1d581b97575c0725655
Худойкулов Муроджон <khudoykulov0000@gmail.com>
2026-02-19 16:03:51 +0700

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,27 @@
-"""Utils module""" 
+"""
+Utils module - вспомогательные утилиты AutoStatAnalysis
+=========================================================
+
+Модуль содержит вспомогательные функции:
+
+Функции:
+    format_price() - форматирование цены
+    format_car_info() - форматирование информации об авто
+    save_to_csv() - сохранение в CSV
+    load_from_csv() - загрузка из CSV
+    CarStatistics - класс для статистического анализа
+"""
+
+from .formatter import format_price, format_car_info
+from .file_handler import save_to_csv, load_from_csv, save_to_json, load_from_json
+from .statistics import CarStatistics
+
+__all__ = [
+    'format_price',
+    'format_car_info',
+    'save_to_csv',
+    'load_from_csv',
+    'save_to_json',
+    'load_from_json',
+    'CarStatistics'
+]
