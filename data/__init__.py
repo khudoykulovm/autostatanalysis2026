c10cbb9c71649d31f77f28168e9a09aba7845a04
Худойкулов Муроджон <khudoykulov0000@gmail.com>
2026-02-19 16:04:17 +0700

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,19 @@
-"""Data module""" 
+"""
+Data module - примеры данных для AutoStatAnalysis
+===================================================
+
+Модуль содержит функции для генерации тестовых данных:
+
+Функции:
+    get_sample_cars() - получить список примеров автомобилей
+    generate_test_data() - сгенерировать тестовые данные
+    load_sample_dataset() - загрузить пример датасета
+"""
+
+from .sample_data import get_sample_cars, generate_test_data, load_sample_dataset
+
+__all__ = [
+    'get_sample_cars',
+    'generate_test_data',
+    'load_sample_dataset'
+]
